635649f85a2283ab79e9d63a9764cecb92abf893
Dave Moore <themoonrat@gmail.com>
2017-11-27 17:46:29 +0000

    removed object deep copying, as it won't be needed in this use case with text style

--- js/utils/deepCopy.js
+++ js/utils/deepCopy.js
@@ -4,8 +4,6 @@ function deepCopy(target, source) {
     for (const prop in source) {
         if (Array.isArray(source[prop])) {
             target[prop] = source[prop].slice();
-        } else if (source[prop] && typeof source[prop] === 'object') {
-            Object.assign(target[prop], source[prop]);
         } else {
             target[prop] = source[prop];
         }
