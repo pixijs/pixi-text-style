3d99febee9db7df035077398813f8778696d8f36
djmisterjon <24865815+djmisterjon@users.noreply.github.com>
2020-01-05 20:46:25 -0500

    Add translate characters encoding
    Easly test fonts, custom fonts with most popular language with special charactere.
    Using free translate.yandex API.

--- src/TextStyleEditor.jsx
+++ src/TextStyleEditor.jsx
@@ -87,7 +87,23 @@ export default class TextStyleEditor {
                         </button>
                     </h3>
                     <Panel id='text' name='Text' selected='true'>
-                        <textarea class='form-control'
+                        <button title='China' class='btn btn-outline-dark btn-file btnTran' onclick={this.translate.bind(this,'zh')}>zh
+                        </button>
+                        <button title='English' class='btn btn-outline-dark btn-file btnTran' onclick={this.translate.bind(this,'en')}>en
+                        </button>
+                        <button title='Russian' class='btn btn-outline-dark btn-file btnTran' onclick={this.translate.bind(this,'ru')}>ru
+                        </button>
+                        <button title='Japanese' class='btn btn-outline-dark btn-file btnTran' onclick={this.translate.bind(this,'ja')}>ja
+                        </button>
+                        <button title='Thai' class='btn btn-outline-dark btn-file btnTran' onclick={this.translate.bind(this,'th')}>th
+                        </button>
+                        <button title='Portuguese' class='btn btn-outline-dark btn-file btnTran' onclick={this.translate.bind(this,'pt')}>pt
+                        </button>
+                        <button title='Arabic' class='btn btn-outline-dark btn-file btnTran' onclick={this.translate.bind(this,'ar')}>ar
+                        </button>
+                        <button title='Czech' class='btn btn-outline-dark btn-file btnTran' onclick={this.translate.bind(this,'cs')}>cs
+                        </button>
+                        <textarea class='form-control'npm run-script build
                             id='input'
                             autofocus='true'
                             oninput={m.withAttr('value', this.onText.bind(this))}
@@ -312,6 +328,37 @@ export default class TextStyleEditor {
         this.indent = parseInt(element.target.value);
         localStorage.indent = this.indent;
         m.redraw();
+    }
+
+    translate(lanKey){
+        if(this._busy){
+            return;
+        }
+        this._busy = true;
+        const url = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
+        const keyAPI = 'trnsl.1.1.20200105T231430Z.d549b6f7b6103206.2c4b8c1983761519c17632b7f640a0ccbdeb2349';
+        const xhr = new XMLHttpRequest();
+        const textAPI = localStorage.getItem('text') || this.text.text;
+        const langAPI = lanKey;
+        const data = 'key='+keyAPI+'&text='+textAPI.substring(0, 16)+'&lang='+langAPI;
+        xhr.open('POST',url,true);
+        xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+        xhr.send(data);
+        xhr.onreadystatechange = ()=>{
+            if (xhr.readyState===4 && xhr.status===200) {
+                const res = xhr.responseText;
+                const json = JSON.parse(res);
+                if(json.code === 200) {
+                    this.text.text = json.text[0];
+                }
+                else {
+                    this.text.text = 'Error Code: ' + json.code;
+                }
+                m.redraw();
+                this.app.render();
+                this._busy = false;
+            }
+        };
     }
 
     onLoadFont(event) {
